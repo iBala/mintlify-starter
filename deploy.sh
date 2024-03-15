ffcffd0143fd8d5e2eb2afc6106f018b3d42a2ce
Vivek <vivek@zorp.one>
2024-03-15 18:31:14 +0530

    clean up

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-git pull origin master
-# npm install
-npm run build
-cd ./build
-aws s3 cp . s3://zorp-docs --recursive
-aws cloudfront create-invalidation --distribution-id E34YHHGSVHH0HY --paths "/*"
-cd ..
